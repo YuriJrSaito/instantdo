f506aea443070bfe66fa3b5c78641b07ee8a1ec2
YuriJrSaito <yurijr.saito@gmail.com>
2023-07-26 19:40:39 -0300

    Drag&Drop completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 "use client";
-import './page.module.css'
 import Header from '@/components/header/index'
 import Panel from '@/components/Panel'
 import Footer from '@/components/Footer'
-import { PanelData } from '@/components/Panel/panelData'
 import { useState } from 'react'
 import { DragDropContext } from 'react-beautiful-dnd';
 
@@ -25,15 +23,54 @@ export default function Home() {
   );
   const [done, setDone] = useState(
     [
-      { id: "done1", text: "Done 1", done: "done1"},
-      { id: "done2", text: "Done 2", done: "done2"},
-      { id: "done3", text: "Done 3", done: "done3"}
+      { id: "done1", text: "Done 1", draggableId: "done1"},
+      { id: "done2", text: "Done 2", draggableId: "done2"},
+      { id: "done3", text: "Done 3", draggableId: "done3"}
     ]
   );
+  const [notDefined, setNotDefined] = useState([]);
 
-  const onDragEnd = (result:any) => {
-    console.log(result);
+  const handleUpdatePanel = async (result:any, array:any, updateArray:any) =>{
+
+    const items = Array.from(array);
+    const [reorderedItem] = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, reorderedItem);
     
+    updateArray(items);
+  }
+
+  const handleNextStage = async (result:any, origin:any, updateOrigin:any, destiny:any, updateDestiny:any) =>{
+    const cOrigin = Array.from(origin);
+    const cDestinty = Array.from(destiny);
+
+    const [reorderedItem] = cOrigin.splice(result.source.index, 1);  
+    updateOrigin(cOrigin);
+
+    cDestinty.splice(result.destination.index, 0, reorderedItem);
+    updateDestiny(cDestinty);
+  }
+
+  const defineOriginDestiny = async (panel:string) =>{
+    let origin_destiny;
+    switch(panel)
+    {
+      case 'panel1': origin_destiny = [todo, setTodo]; break;
+      case 'panel2': origin_destiny = [inProgress, setInProgress]; break;
+      case 'panel3': origin_destiny = [done, setDone]; break; 
+      default: origin_destiny = [notDefined, setNotDefined]; break;
+    } 
+    return origin_destiny;
+  }
+
+  const onDragEnd = async (result:any) => {
+
+      let [origin, updateOrigin] =  await defineOriginDestiny(result.source.droppableId);
+      let [destiny, updateDesnity] =  await defineOriginDestiny(result.destination.droppableId);
+
+      if(result.source.droppableId == result.destination.droppableId)
+        await handleUpdatePanel(result, origin, updateOrigin);
+      else
+        await handleNextStage(result, origin, updateOrigin, destiny, updateDesnity);
   };
 
   return (
@@ -42,11 +79,17 @@ export default function Home() {
             <Header />
             <DragDropContext onDragEnd={onDragEnd}>
               <div className='panels'>
-
                 <Panel key="panel1" title={"A fazer"} color="#B10E5C" data={todo} panelId="panel1"/>
                 <Panel key="panel2" title={"Fazendo"} color="#0047FF" data={inProgress} panelId="panel2"/>
                 <Panel key="panel3" title={"Feito"} color="#0AB061" data={done} panelId="panel3"/>
-
+                {
+                  notDefined.length > 0 &&
+                  <Panel key="panel4" title={"Indefinido"} color="#0AB061" data={notDefined} panelId="panel4"/>
+                }
+                {
+                  notDefined.length == 0 &&
+                  <Panel key="" title="" color="" data={notDefined} panelId=""/>
+                }
               </div>
             </DragDropContext>
           </div>
--- src/components/Panel/index.tsx
+++ src/components/Panel/index.tsx
@@ -9,7 +9,6 @@ const Panel = ({title, color, data, panelId}: {title:string, color:string, data:
         <>        
             {
                 title != "" &&
-                
                 <Droppable droppableId={panelId} key={panelId}>
                     {(provided) => (
                         <ul className={style.panel}>
@@ -18,14 +17,14 @@ const Panel = ({title, color, data, panelId}: {title:string, color:string, data:
                                     <h1 style={{color: `${color}`}}>{title}</h1>
                                 </div>
                             </div>
-                            <div className={style.bottom}>
-                                <div className={style.cards} {...provided.droppableProps} ref={provided.innerRef}>
+                            <div className={style.bottom} {...provided.droppableProps} ref={provided.innerRef}>
+                                <div className={style.cards}>
                                 {
                                     data.map(({id, text}:{id: string, text: string}, index:number) => (
                                         <Card key={id} id={id} text={text} index={index}/>
-                                    ))
+                                    ))  
                                 }
-                                    {provided.placeholder}
+                                {provided.placeholder}
                                 </div>
                             </div>
                         </ul>
